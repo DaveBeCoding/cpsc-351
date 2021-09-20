4f1ce014fa8af548637af9775f2a339c4b7d6de7
nullbitcoder <lxxxl1146@gmail.com>
2021-09-19 19:02:36 -0700

    parent creates child, child creates PS (grandchild)

--- hw-01/example/fork.c
+++ hw-01/example/fork.c
@@ -4,32 +4,57 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 int main()
-{
+    {
     pid_t  pid;/* fork another process */
-    
+
     pid = fork();
-    
+
     if (pid < 0) {
-         
-         /* error occurred */
-         fprintf(stderr, "Fork Failed");
-         
-         exit(-1);
-         
-         }
-         else if (pid == 0) 
-         { /* child process */
-         
-         execlp("/bin/ls", "ls", NULL);
-         
-         }
-         
-         else 
-         { /* parent process *//* parent will wait for the child to complete */
-         
-         wait(NULL);
-         
-         printf("\n Child Complete \n");exit(0);
-         
-         }
-}
+
+        /* error occurred */
+        fprintf(stderr, "Fork Failed");
+
+        exit(-1);
+
+        }
+    else if (pid == 0)
+        { /* child process */
+
+        execlp("/bin/ls", "ls", NULL);
+
+        }
+
+    else
+        { /* parent process *//* parent will wait for the child to complete test*/
+
+        wait(NULL);
+
+        printf("\n Child Complete \n");
+
+        exit(0);
+
+        }
+    }
+
+
+
+
+
+/*
+idea code? resembles above idea.
+
+if (pid == 0) {
+    // child
+    pid = fork();
+    if (pid == 0) {
+        // grandchild (of top-level parent)
+        }
+    else {
+        // parent of the grandchild
+        }
+    }
+else {
+    // parent
+    }
+
+*/
---
+++ hw-01/grand_child.c
@@ -0,0 +1,64 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main()
+    {
+
+    pid_t  pid01, pid02;
+
+	pid01 = fork();
+
+    pid02 = fork();
+    
+    if (pid01 < 0) {
+        
+    /* error occurred */
+    fprintf(stderr, "Fork Failed");
+
+    exit(-1);
+        }
+
+    else if (pid01 > 0 && pid02 > 0) {
+        printf("parent\n");
+		printf("%d %d \n", pid01, pid02);
+		printf(" my id is %d \n", getpid());
+	}
+	else if (pid01 == 0 && pid02 > 0)
+	{
+		printf("First child\n");
+		printf("%d %d \n", pid01, pid02);
+		printf("my id is %d \n", getpid());
+	}
+	else if (pid01 > 0 && pid02 == 0)
+        {
+
+        printf("grandchild\n");
+		printf("%d %d \n", pid01, pid02);
+		printf("my id is %d \n", getpid());
+        execlp("/usr/bin/firefox", "firefox", NULL);
+        }
+    
+    }
+
+
+/*
+idea code?
+
+if (pid == 0) {
+    // child
+    pid = fork();
+    if (pid == 0) {
+        // grandchild (of top-level parent)
+        }
+    else {
+        // parent of the grandchild
+        }
+    }
+else {
+    // parent
+    }
+
+*/
